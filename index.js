e58c56ad176ca936b2926b7d84603abf7a48e932
Karissa McKelvey <krmckelv@gmail.com>
2019-04-29 19:35:39 +0300

    add pollbot

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var DeltaChat = require('deltachat-node')
 var events = require('events')
+var Pollbot = require('pollbot.js')
 
 var args = process.argv.splice(2)
 
 var addr = args[0]
 var mail_pw = args[1]
 var cwd = args[2] || process.cwd()
+var locale = 'en'
+var bot = new Pollbot(locale)
 
 var dc = new DeltaChat()
 dc.configure({ addr, mail_pw })
